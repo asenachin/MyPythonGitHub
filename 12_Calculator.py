61d3abff16ade9ecc882e4d4a244f39fe7a7a988
asenachin <senachin.ap@yandex.ru>
2022-07-15 09:25:14 +0700

    Добавление кнопки

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -9,10 +9,17 @@ first = tk.Label(text = "Первое число")
 first.grid(column=0, row=0, padx=10, pady=10)
 second = tk.Label(text = "Второе число")
 second.grid(column=0, row=1)
+result = tk.Label(text = "Результат")
+result.grid(column=0, row=2, padx=10, pady=10)
 
 firstEntry = tk.Entry()
 firstEntry.grid(column=1, row=0)
 secondEntry = tk.Entry()
 secondEntry.grid(column=1, row=1)
+resultLabel = tk.Label(text = "")
+resultLabel.grid(column=1, row=2)
+
+button = tk.Button(window, text="Вычислить сумму")
+button.grid(column=1, row=3)
 
 window.mainloop()
