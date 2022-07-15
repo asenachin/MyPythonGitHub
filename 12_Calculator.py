840ee8af3b829f1a9853361e157974b7c172010c
asenachin <senachin.ap@yandex.ru>
2022-07-15 09:12:23 +0700

    Добавление полей ввода

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -1,7 +1,18 @@
+# ~ Python калькулятор
 import tkinter as tk
 
 window=tk.Tk()
 window.geometry("320x200")
 window.title("Python калькулятор")
+
+first = tk.Label(text = "Первое число")
+first.grid(column=0, row=0, padx=10, pady=10)
+second = tk.Label(text = "Второе число")
+second.grid(column=0, row=1)
+
+firstEntry = tk.Entry()
+firstEntry.grid(column=1, row=0)
+secondEntry = tk.Entry()
+secondEntry.grid(column=1, row=1)
 
 window.mainloop()
